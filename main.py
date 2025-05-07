b3346c254ba30f2bf0d12f4bc7becc3b138f2fa1
ahegermark <ahegermark@gmail.com>
2025-05-07 23:10:17 +0200

    Code update and gitignore

--- main.py
+++ main.py
@@ -2,9 +2,25 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
-    print("Starting Asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids")
+
+    # Game loop
+    # This is the main loop that runs the game
+    # It handles events, updates the game state, and draws the game to the screen
+    # It runs until the user quits the game
+    while True:
+        screen.fill("black")
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+                
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
