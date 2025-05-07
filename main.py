73a5b70283c9b17f71d6d27bbab3a51b7f75075e
ahegermark <ahegermark@gmail.com>
2025-05-07 23:53:38 +0200

    Update player sprit

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_caption("Asteroids")
+    clock = pygame.time.Clock()
 
-    # Game loop
-    # This is the main loop that runs the game
-    # It handles events, updates the game state, and draws the game to the screen
-    # It runs until the user quits the game
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
-        screen.fill("black")
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-                
+
+        updatable.update(dt)
+        
+        screen.fill("black")
+        
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
